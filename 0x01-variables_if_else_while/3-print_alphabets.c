d656eed46b93cf0e5d146d51cd7808522f1704b8
Hundred04 <oluwagbaderoogunmefun@gmail.com>
2023-06-16 12:24:00 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main-Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+	char C = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (C <= 'Z')
+	{
+		putchar(C);
+		C++;
+	}
+	putchar('\n');
+	return (0);
+}
